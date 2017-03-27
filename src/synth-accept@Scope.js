44325086e1cc91adee598ae575650e1c1e6361cc
FailorTwist <itelmen@yahoo.ie>
2017-03-27 09:36:38 -0400

    commit before rewriting accessSynth

--- src/synth-accept@Scope.js
+++ src/synth-accept@Scope.js
@@ -1,40 +1,39 @@
-this.acceptsName = function(name, acceptMode) {
+this.acceptsName_m = function(mname, acceptMode) {
   if (this.isScript() || this.isModule())
-    return this.acceptsName_script(name, acceptMode);
+    return this.acceptsName_m_script(mname, acceptMode);
   if (this.isAnyFnBody())
-    return this.acceptsName_fn(name, acceptMode);
+    return this.acceptsName_m_fn(mname, acceptMode);
 
   ASSERT.call(this, false,
     '<'+this.typeString()+'> unsupported');
 };
 
-this.acceptsName_script = function(name, m) {
+this.acceptsName_m_script = function(mname, m) {
   ASSERT.call(this, m === ACC_DECL,
     'a script scope will only respond to <accept-decl> requests, no <accept-refs>');
 
   if (this.synthNamesUntilNow === null)
     this.calculateBaseSynthNames_script();
 
-  if (this.containsSynthName(name))
+  if (this.containsSynthName_m(mname))
     return false;
 
   return true;
 };
 
-this.acceptsName_fn = function(name, m) {
+this.acceptsName_m_fn = function(mname, m) {
   if (this.synthNamesUntilNow === null)
     this.calculateBaseSynthNames_fn();
 
   var argList = this.funcHead;
 
-  if (this.containsSynthName(name))
+  if (this.containsSynthName_m(mname))
     return false;
 
-  if (argList.scopeName &&
-    name === argList.scopeName.name)
+  if (argList.hasScopeName_m(mname))
     return false;
 
-  if (name === 'arguments') {
+  if (name === _m('arguments')) {
     if (m === ACC_REF)
       return false;
     if (this.firstNonSimple !== null)
@@ -47,10 +46,14 @@ this.acceptsName_fn = function(name, m) {
   }
 
   if (m === ACC_DECL) {
-    var ref = argList.findRef(name);
+    var ref = argList.findRef_m(mname);
     if (ref && !ref.resolved)
       return false;
   }
 
   return true;
+};
+
+this.acceptsName = function(name, acceptMode) {
+  return this.acceptsName_m(_m(name), acceptMode);
 };
---
+++ src/synth-alloc-name@Scope.js
@@ -0,0 +1,20 @@
+this.newSynthName = function(baseName, locrs) {
+  locrs = locrs || null;
+  var i = 0, name = baseName;
+
+  RENAME:
+  for (;; ++i, name = baseName + "" + i) {
+    var mname = _m(name);
+    if (!this.acceptsName_m(mname, ACC_DECL))
+      continue RENAME;
+
+    if (locrs) {
+      var l = 0;
+      while (l < locrs.length)
+        if (!locrs[l++].acceptsName_m(mname, ACC_REF))
+          continue RENAME;
+    }
+  }
+
+  return name;
+};
