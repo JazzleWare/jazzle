90410b5b83ce1336cd5b9b836039a78a14c39ae1
FailorTwist <itelmen@yahoo.ie>
2017-06-17 05:13:29 -0400

    minor edits

--- src/satisfy@SourceScope.js
+++ src/satisfy@SourceScope.js
@@ -17,7 +17,7 @@ function(bundler) {
     if (this.asMod.mns.has(_m(name)))
       this.asMod.mns.set(_m(name), m['#scope']);
 
-    m['#scope'].satisfyAll(mim.at(e));
+    m['#scope'].satisfyAll(mim.at(e), loni, bundler);
 
     bundler.path = oPath;
     e++;
@@ -26,22 +26,47 @@ function(bundler) {
   return loni;
 };
 
+this.findSatisfierEntry_m =
+function(mname, loni, bundler) {
+  var entry = this.findExportedEntry_m(mname);
+  if (entry === null) 
+    entry = this.findInForwardEntries_m(mname);
+  return entry;
+};
+
 this.satisfyAll =
-function(list) {
+function(list, bundler) {
   var mns = this.asMod.mns, e = 0, len = list.length();
   while (e < len) {
     var mname = list.keys[e];
     var entry = this.findExportedEntry_m(mname);
-    if (entry === null) {
-      var l = 0, mnsLen = mns.length();
-      while (l < mnsLen)
-        if (entry = mns.at(l++).findExportedEntry_m(mname))
-          break;
-    }
+    if (entry === null)
+      entry = this.findInForwardEntries_m(mname, bundler);
     if (entry === null)
       this.err('unsatisfied.import');
     var im = list.at(e);
     im === entry.target /* a.js: import {e as a} from './a.js'; export let e = 5; */ || im.referTo(entry.target);
     e++;
+  }
+};
+
+this.findInForwardEntries_m =
+function(mname, loni, bundler) {
+  var mns = this.asMod.mns, e = 0, len = mns.length();
+  while (e < len) {
+    var satisfierNamespace = mns.at(e);
+    if (satisfierNamespace === null) {
+      var name = _u(mns.keys[e]);
+      var oPath = bundler.cd(pathFor(name));
+      var curName = tailFor(name)
+      if (bundler.has(curName))
+        satisfierNamespace = bundler.get(curName);
+      else {
+        satisfierNamespace = bundler.load(curName);
+        loni.push(satisfierNamespace);
+      }
+      mns.set(mns.keys[e], satisfierNamespace );
+    }
+    var entry = satisfier
   }
 };
