bd74036fbc69c36ff4a47afdb62ad7e422d11c41
FailorTwist <itelmen@yahoo.ie>
2017-09-05 15:31:28 -0400

    cvtz

--- src/new-transform-BinaryExpression@Transformer.js
+++ src/new-transform-BinaryExpression@Transformer.js
@@ -1,5 +1,14 @@
-Transformers['LogicalExpression'] = Transformers['BinaryExpression'] =
-function(n, ownerList, isVal) {
+Transformers['LogicalExpression'] =
+function(n, isVal) {
+  n.left = this.tr(n.left, true);
+  var cvtz = this.setCVTZ(createObj(this.cvtz));
+  n.right = this.tr(n.right, true);
+  this.setCVTZ(cvtz );
+  return n;
+};
+
+Transformers['BinaryExpression'] =
+function(n, isVal) {
   n.left = this.tr(n.left, true);
   n.right = this.tr(n.right, true);
   return n;
