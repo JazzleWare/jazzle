56f467268cf17b95573337541b5ac2b42583e446
FailorTwist <itelmen@yahoo.ie>
2017-11-05 09:25:39 -0500

    minor fixes

--- src/new-uemit-export@Emitter.js
+++ src/new-uemit-export@Emitter.js
@@ -2,7 +2,7 @@
 function(n, isVal) {
   var b = n['#binding'];
   var elem = n.declaration;
-  if (b !== null) {
+  if (b !== null) { // if it has to have a binding, then it's either an expression or a nameless fn or cls
     this.wt('var',ETK_ID).bs();
     this.w(b.synthName).os().w('=').os();
     this.eN(elem, EC_NONE, false).w(';');
